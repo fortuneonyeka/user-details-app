c22088ca2b3ac06f5f2585589f7afdf20897c1dc
fortunatus <ihedfortune@gmail.com>
2024-10-09 09:37:16 +0100

    install dependences

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import UserList from './components/UserList';
+import UserDetails from './components/UserDetails';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App min-h-screen bg-gray-100">
+        <Routes>
+          <Route path="/" element={<UserList />} />
+          <Route path="/user/:id" element={<UserDetails />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
